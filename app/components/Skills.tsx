5557380fa24bd39ba7a5389edb9491702d033048
jeff <abdallahjeff92@gmail.com>
2026-04-13 16:41:38 +0100

    change the intro2

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -2,30 +2,43 @@ import React, { useEffect, useState } from "react";
 import lodash from "lodash";
 import { Flipped, Flipper } from "react-flip-toolkit";
 import {
-  SiRemix,
-  SiHtml5,
-  SiJavascript,
+  SiC,
+  SiCplusplus,
   SiTypescript,
-  SiExpress,
-  SiVite,
-  SiMui,
-  SiTailwindcss,
+  SiNestjs,
+  SiPostgresql,
+  SiDocker,
+  SiKubernetes,
+  SiWordpress,
+  SiGit,
   SiGithub,
-  SiAntdesign,
+  SiPrisma,
+  SiJsonwebtokens,
+  SiLinux,
+  SiJest,
+  SiGithubactions,
 } from "react-icons/si";
-import { TbBrandNextjs, TbBrandRedux } from "react-icons/tb";
-import { RiCss3Fill } from "react-icons/ri";
-import { FaReact, FaNodeJs, FaNpm } from "react-icons/fa";
-import { BiLogoMongodb } from "react-icons/bi";
+import { TbBrandNextjs } from "react-icons/tb";
+import { FaNetworkWired } from "react-icons/fa";
 import { CgVercel } from "react-icons/cg";
 import { LinkPreview } from "./ui/link-preview";
 
 const Skills: React.FC = () => {
   const skillItems = [
     {
-      title: "React",
-      icon: <FaReact className="text-blue-500" />,
-      link: "https://reactjs.org/",
+      title: "C",
+      icon: <SiC className="text-blue-600" />,
+      link: "https://en.wikipedia.org/wiki/C_(programming_language)",
+    },
+    {
+      title: "C++",
+      icon: <SiCplusplus className="text-blue-700" />,
+      link: "https://isocpp.org/",
+    },
+    {
+      title: "TypeScript",
+      icon: <SiTypescript className="text-blue-500" />,
+      link: "https://www.typescriptlang.org/",
     },
     {
       title: "Next.js",
@@ -33,54 +46,24 @@ const Skills: React.FC = () => {
       link: "https://nextjs.org/",
     },
     {
-      title: "Redux",
-      icon: <TbBrandRedux className="text-purple-600" />,
-      link: "https://redux.js.org/",
+      title: "NestJS",
+      icon: <SiNestjs className="text-red-600" />,
+      link: "https://nestjs.com/",
     },
     {
-      title: "JavaScript",
-      icon: <SiJavascript className="text-yellow-500" />,
-      link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+      title: "PostgreSQL",
+      icon: <SiPostgresql className="text-blue-400" />,
+      link: "https://www.postgresql.org/",
     },
     {
-      title: "TypeScript",
-      icon: <SiTypescript className="text-blue-600" />,
-      link: "https://www.typescriptlang.org/",
+      title: "Docker",
+      icon: <SiDocker className="text-blue-500" />,
+      link: "https://www.docker.com/",
     },
     {
-      title: "HTML5",
-      icon: <SiHtml5 className="text-orange-500" />,
-      link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
-    },
-    {
-      title: "CSS3",
-      icon: <RiCss3Fill className="text-blue-500" />,
-      link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-    },
-    {
-      title: "Node.js",
-      icon: <FaNodeJs className="text-green-500" />,
-      link: "https://nodejs.org/",
-    },
-    {
-      title: "Express",
-      icon: <SiExpress className="text-black dark:text-white" />,
-      link: "https://expressjs.com/",
-    },
-    {
-      title: "Vite",
-      icon: <SiVite className="text-purple-500" />,
-      link: "https://vitejs.dev/",
-    },
-    {
-      title: "Remix",
-      icon: <SiRemix className="text-cyan-500" />,
-      link: "https://remix.run/",
-    },
-    {
-      title: "MongoDB",
-      icon: <BiLogoMongodb className="text-green-600" />,
-      link: "https://www.mongodb.com/",
+      title: "Kubernetes",
+      icon: <SiKubernetes className="text-blue-600" />,
+      link: "https://kubernetes.io/",
     },
     {
       title: "Vercel",
@@ -88,29 +71,49 @@ const Skills: React.FC = () => {
       link: "https://vercel.com/",
     },
     {
-      title: "Material UI",
-      icon: <SiMui className="text-blue-600 " />,
-      link: "https://mui.com/",
+      title: "WordPress",
+      icon: <SiWordpress className="text-blue-800" />,
+      link: "https://wordpress.org/",
     },
     {
-      title: "Tailwind Css",
-      icon: <SiTailwindcss className="text-cyan-400" />,
-      link: "https://tailwindcss.com/",
+      title: "Git",
+      icon: <SiGit className="text-orange-500" />,
+      link: "https://git-scm.com/",
     },
     {
-      title: "Github",
+      title: "GitHub",
       icon: <SiGithub className="text-black dark:text-white" />,
       link: "https://github.com/",
     },
     {
-      title: "Npm",
-      icon: <FaNpm className="text-red-600" />,
-      link: "https://www.npmjs.com/",
+      title: "REST APIs",
+      icon: <FaNetworkWired className="text-green-500" />,
+      link: "https://restfulapi.net/",
     },
     {
-      title: "Ant Design",
-      icon: <SiAntdesign className="text-blue-500" />,
-      link: "https://ant.design/",
+      title: "Prisma",
+      icon: <SiPrisma className="text-black dark:text-white" />,
+      link: "https://www.prisma.io/",
+    },
+    {
+      title: "JWT",
+      icon: <SiJsonwebtokens className="text-pink-500" />,
+      link: "https://jwt.io/",
+    },
+    {
+      title: "Linux",
+      icon: <SiLinux className="text-yellow-500" />,
+      link: "https://www.linux.org/",
+    },
+    {
+      title: "Jest",
+      icon: <SiJest className="text-red-500" />,
+      link: "https://jestjs.io/",
+    },
+    {
+      title: "GitHub Actions",
+      icon: <SiGithubactions className="text-blue-500" />,
+      link: "https://github.com/features/actions",
     },
   ];
 
